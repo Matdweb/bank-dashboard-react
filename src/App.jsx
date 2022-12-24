8373d1989203e53584f110032551ef05a219b68f
Matias Vargas <mat.dweb28[1>
2022-12-23 22:41:57 -0600

    Responsive adaptations and Charts integrations --Mat_dweb

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import DashBoard from './components/DashBoard/DashBoard'
 import './App.css'
 import Profile from './components/Profile/Profile'
 
+export const u = 12
+
 function App() {
   return (
     <div className="father-app">
--- src/components/BarsGraphic/BarsGraphic.jsx
+++ src/components/BarsGraphic/BarsGraphic.jsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Progress } from '@chakra-ui/react'
 import { Tooltip } from '@chakra-ui/react'
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
-import { Button} from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
 import GraphicCSS from './BarsGraphic_styles.module.css'
 
 import { IoIosArrowDown } from 'react-icons/all'
@@ -14,8 +14,52 @@ import {
     MenuItem,
   } from '@chakra-ui/react'
 
-function BarsGraphic() {
+import { PieChart, Pie, ResponsiveContainer } from 'recharts';
+import { Radar, RadarChart, PolarGrid, PolarAngleAxis, PolarRadiusAxis } from 'recharts';
 
+  //Circular chart data 
+  const data = [
+    { name: 'Group A', value: 400 },
+    { name: 'Group B', value: 300 },
+    { name: 'Group C', value: 300 },
+    { name: 'Group D', value: 200 },
+  ];
+ //Radar chart data
+  const billsCategories = [
+    {
+      subject: 'School',
+      A: 120,
+      fullMark: 150,
+    },
+    {
+      subject: 'Trips',
+      A: 98,
+      fullMark: 150,
+    },
+    {
+      subject: 'English',
+      A: 86,
+      fullMark: 150,
+    },
+    {
+      subject: 'University',
+      A: 119,
+      fullMark: 150,
+    },
+    {
+      subject: 'Leisure',
+      A: 85,
+      fullMark: 150,
+    },
+    {
+      subject: 'Food',
+      A: 65,
+      fullMark: 150,
+    },
+  ];
+
+function BarsGraphic() {
+  //First Chart
     const values = ['$709','$458','$600','$854','$678'] 
     const bars = []
     const optionsMenu = ['Montly','Per year','Dialy','Every 1h']
@@ -29,11 +73,20 @@ function BarsGraphic() {
       bars.push(height)
     })
 
+    // Responsive configuration for graphics container
+    const [containerWidth, setContainerWidth] = useState(350)
+    useEffect(() => {
+      window.screen.width <= 600 ? setContainerWidth(250) : setContainerWidth(350)
+    }, []);
+
+    
   return (
     <div className={GraphicCSS.graphic}>
           <label className={GraphicCSS.graphic_menu}>
             <Menu>
                 <MenuButton
+                position='absolute'
+                marginBottom='220px'
                 px={4}
                 py={2}
                 transition='all 0.2s'
@@ -41,9 +94,7 @@ function BarsGraphic() {
                 borderWidth='0px'
                 backgroundColor='rgb(25, 25, 30)'
                 color='#fff'
-                margin='10px'
                 padding='10px'
-                marginBottom='10px'
                 height='25px'
                 _focus={{ boxShadow: 'outline' }}
                 _active={{ background:'rgb(37, 37, 45)' }}
@@ -66,7 +117,7 @@ function BarsGraphic() {
                 </MenuList>
               </Menu>
 
-            <Tabs colorScheme='rgb(110,112,125)'>
+            <Tabs width={containerWidth} height='400px' colorScheme='rgb(110,112,125)'>
               <TabList
               border='none'
               margin='10px'>
@@ -75,37 +126,56 @@ function BarsGraphic() {
                 <Tab>Three</Tab>
               </TabList>
               <TabPanels>
-                <TabPanel>
+                <TabPanel width='100%' height='380px'>
+                  {/* //Graphic #1 */}
+                  <div className={GraphicCSS.graphics_container}>
+                    {
+                      bars.map((height,index)=>{
+                        return (
+                        <Tooltip key={index} 
+                        label={values[index]} 
+                        fontSize='md'
+                        placement='top'>
+                          <Progress 
+                          value={0}  
+                          className={GraphicCSS.progress_bar} 
+                          style={
+                            {height: `${height-10}%`}
+                          }/>
+                        </Tooltip>
+                        )
+                      })
+                    }
+                  </div>
 
                 </TabPanel>
-                <TabPanel>
-                  <p>two!</p>
+                <TabPanel width='100%' height='380px'>
+                {/* //Graphic #2 */}
+                  <div style={{ width: '100%', height: 300 }}>
+                    <ResponsiveContainer>
+                      <PieChart>
+                        <Pie dataKey="value" data={data} fill="#2b53af" label />
+                      </PieChart>
+                    </ResponsiveContainer>
+                  </div>
+
                 </TabPanel>
-                <TabPanel>
-                  <p>three!</p>
+                <TabPanel width='100%' height='380px'>
+                {/* //Graphic #3 */}
+                <ResponsiveContainer width="100%" height="100%">
+                  <RadarChart cx="50%" cy="50%" outerRadius="80%" data={billsCategories}>
+                    <PolarGrid />
+                    <PolarAngleAxis dataKey="subject" />
+                    <PolarRadiusAxis />
+                    <Radar name="Mike" dataKey="A" stroke="#5d76ae" fill="#2b53af" fillOpacity={0.6} />
+                  </RadarChart>
+                </ResponsiveContainer>
+
                 </TabPanel>
               </TabPanels>
             </Tabs>
           </label>
-          <div className={GraphicCSS.graphics_container}>
-            {
-              bars.map((height,index)=>{
-                return (
-                <Tooltip key={index} 
-                label={values[index]} 
-                fontSize='md'
-                placement='top'>
-                  <Progress 
-                  value={0}  
-                  className={GraphicCSS.progress_bar} 
-                  style={
-                    {height: `${height-10}%`}
-                  }/>
-                </Tooltip>
-                )
-              })
-            }
-          </div>
+          
     </div>
   )
 }
